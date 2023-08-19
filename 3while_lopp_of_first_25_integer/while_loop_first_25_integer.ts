cee6023eeb27860a6b22b5c8fc39880e5b8e9ce0
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-19 16:29:23 +0700

    while loop to print 25  integers

---
+++ 3while_lopp_of_first_25_integer/while_loop_first_25_integer.ts
@@ -0,0 +1,8 @@
+// Write a program that uses a while loop to print the first 25 integers.
+console.log("===Program to Print First 25 numbers===");
+
+var given_number = 0
+while (given_number <= 25){
+    console.log(given_number);
+    var given_number = given_number + 1
+}
