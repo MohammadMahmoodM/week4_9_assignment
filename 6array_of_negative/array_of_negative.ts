a121d83ef15430ec06b46ae483b18bd1b43cdc95
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-19 16:32:16 +0700

    remove negative numbers from array using while loop or for loop

---
+++ 6array_of_negative/array_of_negative.ts
@@ -0,0 +1,15 @@
+// Write a program having an array of numbers if the number is
+// negative it should remove the negative number from the array.
+
+var aray_of_num: number[] = [1,4,-10,5,-3,8,6,9,-4]
+
+console.log("Before Removing Negative number", aray_of_num);
+
+aray_of_num.forEach(num => {
+    if (num < 0){
+        var index_of_negative = aray_of_num.indexOf(num)
+        aray_of_num.splice(index_of_negative, 1)
+    }
+});
+
+console.log("After Removing Negative number", aray_of_num);
